374b39ae21be9498071e24dde3d99ad511d68b4b
eternalmgy01 <60344310+eternalmgy01@users.noreply.github.com>
2021-05-05 20:08:59 +0300

    lab10

---
+++ lib/pages/second_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class User {
+  String name;
+  String phone;
+  String email;
+  String country;
+  String story;
+}
+
+class UserInfoPage extends StatelessWidget {
+
+  User userInfo;
+  UserInfoPage({this.userInfo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Info'),
+        centerTitle: true,
+      ),
+      body: Card(
+        margin: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                '${userInfo.name}',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              subtitle: Text('${userInfo.story}'),
+              leading: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              trailing: Text('${userInfo.country}'),
+            ),
+            ListTile(
+              title: Text(
+                '${userInfo.phone}',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: Icon(
+                Icons.phone,
+                color: Colors.black,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                '${userInfo.email.isEmpty ? 'Not specified' : userInfo.email}',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: Icon(
+                Icons.mail,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
